7a79b4a9c4ae6feb3faa8b251fcbe4b799d9acbc
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-27 17:43:07 +0100

    push

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,16 +1,12 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 
-// numero = parseInt(prompt('Inserire un numero'));
-// cubo = Math.pow(numero, numero);
-// console.log(cubo);
-
+let container = document.getElementById('risultati');
 let numero = parseInt(prompt('Inserire un numero'));
 let cubo = 0;
 i=0;
 while (i <= numero) {
-    cubo = Math.pow(numero,numero)
-    console.log('Il cubo di ' + i + ' è ' + );
+    cubo = Math.pow(i,3)
+    console.log('Il cubo di ' + i + ' è ' + cubo);
+    container.innerHTML += "<div class='risultati'>" + 'Il cubo di ' + i + ' è ' + cubo + "</div>";
     i++;
-    
 }
-
--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,12 +1,9 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
+const numero = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 let somma = 0;
-let i = 0;
-
-while (i <= 9){
-    numero = parseInt(prompt('Inserire il ' + (i+1) + 'º numero'));
-    somma = somma + numero;
-    i++;   
+for (let i = 0; i <= numero.length; i++) {
+    somma = i + i;
+    console.log('La somma di ' + i + ' + ' + i + ' è = a ' + somma);
 }
 
-alert("La somma dei numeri è " + somma + " La media dei numeri inseriti è " + (somma / [i]))
