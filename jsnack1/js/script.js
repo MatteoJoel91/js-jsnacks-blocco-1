814a989d07c2a6091c9af34485e4dac716c2e1f3
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-27 15:12:36 +0100

    push

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,3 +1,12 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero. 
 // Il programma stampa la somma di tutti i numeri inseriti.
 
+
+let numeri = parseInt(prompt('Inserire un numero'));
+
+let i = 0;
+
+while (i <= 8){
+    numeri = parseInt(prompt('Inserire un numero'));
+    i++;
+}
