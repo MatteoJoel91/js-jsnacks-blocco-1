6b5648d76ec1f57e7a36c552b046b370866edea2
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-27 18:48:48 +0100

    push

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,6 +1,7 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
 let container = document.getElementById('risultati');
+let elementi = document.getElementById('numeri');
 
 let numero_giri = 10;
 
@@ -11,13 +12,14 @@ let media = 0;
 for (let i = 0; i <= numero_giri; i++) {
     somma = i + somma;
     console.log(i);
-    container.innerHTML += "<div class='risultati'>" + i + "</div>";
+    elementi.innerHTML += i + ' ' ;
 }
+
 
 media = somma / numero_giri;
 
 console.log('La somma dei primi 10 numeri è ' + somma);
-container.innerHTML += "<div class='risultati'>" + 'La somma di questi numeri è ' + somma + "</div>";
+container.innerHTML += "<p>" + 'La somma di questi numeri è ' + somma + "</p>";
 
 console.log ('La media della somma dei primi 10 numeri è ' + media);
-container.innerHTML += "<div class='risultati'>" + 'La media della somma di questi numeri è ' + media + "</div>";
+container.innerHTML += "<p>" + 'La media della somma di questi numeri è ' + media + "</p>";
